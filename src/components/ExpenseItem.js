8087161cda16c69e1f039cc1e52d91b145c3defb
Ajiqi <yuhaziq@gmail.com>
2021-05-15 05:37:51 +0800

    Added nested div in ExpenseItem component and style with css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return <h2>Expense item!</h2>
+  return (
+    <div className="expense-item">
+      <div>May 15th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">RM 250</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
