8d79cba6bd13805a3d1dc0ce8098f75559d0cc9c
Ajiqi <ajiq.zaid@gmail.com>
2021-05-19 00:17:38 +0800

    Added changed event handler in ExpenseForm

--- src/components/newExpenses/ExpenseForm.js
+++ src/components/newExpenses/ExpenseForm.js
@@ -1,10 +1,25 @@
+import React, { useState } from "react";
+
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    const titlechangeHandler = (event) => {
-        console.log(event.target.value);
-    }
-    return (
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titlechangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
@@ -13,11 +28,16 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" />
+          <input type="number" min="0.01" onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2021-01-01" max="2023-12-31" />
+          <input
+            type="date"
+            min="2021-01-01"
+            max="2023-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
